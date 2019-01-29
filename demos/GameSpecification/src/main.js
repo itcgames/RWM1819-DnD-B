72df78fe5b0e8dc52d9f6846622b4c2afc12822b
John O'Grady <johndj.og@gmail.com>
2019-01-29 10:21:54 +0000

    finishied final feature implementation

--- demos/GameSpecification/src/main.js
+++ demos/GameSpecification/src/main.js
@@ -21,19 +21,6 @@ class Main
     this.ctx = this.canvas.getContext("2d");
     document.body.appendChild(this.canvas);
 
-    this.audioManager = new SoundManager();
-    this.audioManager.initialize();
-
-    var sounds = {
-      'pickup': 'resources/sound/pickup.wav', 
-      'error' : 'resources/sound/error.wav',
-      'confirm':'resources/sound/confirmation.wav'};
-
-    for(name in sounds)
-    {
-        this.audioManager.loadSound(name, sounds[name]);
-    }
-
     this.dragManager = new DragDropManager();
 
     this.drag1 = new Square(200,400,50,50, 'red', this.audioManager, "drag");
---
+++ demos/GameSpecification/src/math.js
@@ -0,0 +1,31 @@
+var utilities = {
+
+    // simple collision check to see if a point is within a box, used to detect mouse
+    pointBoxCollision: function(box, point){
+        if(point.x > box.x && 
+            point.x < box.x + box.width && 
+            point.y > box.y && 
+            point.y < box.y + box.height)
+        {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    // simple collision check to see if a box is touching another box, used to verify collision
+    boundingBoxCollision: function(box_a, box_b){
+        if(box_a.x < box_b.x + box_b.width &&
+            box_a.x + box_a.width > box_b.x &&
+            box_a.y < box_b.y + box_b.height &&
+            box_a.height + box_a.y > box_b.y){
+                return true;
+            } else {
+                return false;
+            }
+    },
+
+    lerp: function(start, end, t){
+        return (start + t *(end - start));
+    }
+}
